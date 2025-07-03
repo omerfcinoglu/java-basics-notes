8710c654c0718b875ddf5b07e23dc61a9dc87c49
Omer Faruk Cinoglu <62906173+omerfcinoglu@users.noreply.github.com>
2025-07-03 17:44:26 +0300

    Update Main.java

--- pet_owner_example/Main.java
+++ pet_owner_example/Main.java
@@ -1,19 +1,35 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-      Cat cat = new Cat("kedy", 4);
-        Dog dog = new Dog("köpke", 2);
-        Owner o1 = new Owner("omer", cat);
-        Owner o2 = new Owner("faruk", dog);
-        
-        o1.interactWithPet();
-        o2.interactWithPet();
-        
-        o1.introduce();
-        o2.introduce();
-        
-        dog.makeSound();
-        cat.makeSound();
+  public static void main(String[] args) {
+    Cat cat = new Cat("kedy", 4);
+    Dog dog = new Dog("köpke", 2);
+    Owner o1 = new Owner("omer", cat);
+    Owner o2 = new Owner("faruk", dog);
+
+    o1.interactWithPet();
+    o2.interactWithPet();
+
+    o1.introduce();
+    o2.introduce();
+
+    dog.makeSound();
+    cat.makeSound();
   }
 }
+
+/*
+ * 
+ * Output:
+ * 
+ * Kitty plays with bla bla
+ * Doggy plays with bla bla
+ * Owner Info - Owner name: omer
+ * Animal Info - Age: 4 Name: kedy type is Cat
+ * Owner Info - Owner name: faruk
+ * Animal Info - Age: 2 Name: köpke type is Dog
+ * köpke says: Woof!
+ * kedy says: Meow!
+ * 
+ * 
+ */
